3fb829fe97eedf985d1e5b76256aaf332d85e539
vytcka <c4002911@newcastle.ac.uk>
2026-03-14 15:57:37 -0800

    Instantiated a way for the front end developers ot manage around developing different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Check from "./Check";
 
 function App() {
-
-  const [response, setResponse] = useState("");
-
-  useEffect(() => {
-    fetch("/", {
-      credentials: "include"
-    })
-      .then(res => res.text())
-      .then(data => setResponse(data))
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <BrowserRouter>
 
-        <p>
-          {/*Returns index.html, that means it works... */}
-          {(response)} hello world !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Check">Check</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Check" element={<Check />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Home.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    fetch("/", {
+      credentials: "include"
+    })
+      .then(res => res.text())
+      .then(data => setResponse(data))
+  }, []);
+
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <p>
+          {/*Returns index.html, that means it works... */}
+          {(response)} Home page working working. !!!
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
