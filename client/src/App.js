fe709488f95b0d8c1706f5d0f1f19de7e720a6b1
Mira <mangporyantavej@gmail.com>
2026-03-18 10:43:38 +0000

    Added signup and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Check from "./Check";
+import Signup from "./signup";
+import Login from "./login";
 
 function App() {
   return (
@@ -9,11 +11,15 @@ function App() {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/Check">Check</Link>
+        <Link to="/Signup">Sign Up</Link>
+        <Link to="/Login">Login</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Check" element={<Check />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
 
     </BrowserRouter>
