36ab368dd5888649c529a7a8752e31a9a9222086
Mira <mangporyantavej@gmail.com>
2026-03-18 10:43:38 +0000

    Added signup and login page

---
+++ client/src/signup.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import './form.css'; 
+import { Link } from 'react-router-dom';
+
+export default function Signup() {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    //validation and submit form data to backend
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (formData.password !== formData.confirmPassword) {
+            setErrorMessage('Passwords do not match');
+            return;
+        }
+        else {
+            setErrorMessage('');
+            setSuccessMessage('Signup successful!');
+        }
+    };
+
+    //UI
+    return (
+        <div className="form-container">
+            <h1>Sign Up</h1>
+            <h2>Please create an account</h2>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder='username'
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder='email'
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder='password'
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        placeholder='confirm password'
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                {errorMessage && <p className="error">{errorMessage}</p>}
+                {successMessage && <p className="success">{successMessage}</p>}
+                <button type="submit">Submit</button>
+
+                <Link to="/request" className="guest">Continue as guest?</Link>
+                <p>Already have an account? <Link to="/Login">Login</Link></p>
+
+            </form>
+        </div>
+    );
+
+}
