e0509720eaecb0f358b9c3c80b2ea1e96163afbc
Mira <mangporyantavej@gmail.com>
2026-04-22 15:36:10 +0100

    Add search bar and filters for doctor review page

---
+++ client/src/doctorItems.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        id: 1,
+        name:"Dr.John Smith",
+        specialty:"Cardiology",
+        rating: 5,
+        gender:"Male",
+        language: ["English", "Spanish", "French"],
+        location: "Royal Victoria Infirmary"
+    },
+    {
+        id: 2,
+        name:"Dr.Alice Smith",
+        specialty:"Pediatrics",
+        rating: 3,
+        gender:"Male",
+        language: ["English", "Chinese", "Italian"],
+        location: "Great North Children's Hospital"
+    },
+    {
+        id: 3,
+        name:"Doctor 3",
+        specialty:"Cardiology",
+        rating: 2,
+        gender:"Male",
+        language: ["English", "Spanish"],
+        location: "Royal Victoria Infirmary"
+    },
+    {
+        id: 4,
+        name:"Doctor 4",
+        specialty:"Dermatology",
+        rating: 5,
+        gender:"Female",
+        language: ["English", "Vietnamese"],
+        location: "Freeman Hospital"
+    },
+    {
+        id: 5,
+        name:"Doctor 5",
+        specialty:"Dermatology",
+        rating: 4,
+        gender:"Female",
+        language: ["English"],
+        location: "Royal Victoria Infirmary"
+    },
+]
