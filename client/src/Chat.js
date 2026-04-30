6edd15c5fa6bc059b5c64e431427a881bfe8d17a
vytcka <c4002911@newcastle.ac.uk>
2026-04-30 05:43:30 -0800

    example

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -10,6 +10,7 @@ export default function Chat() {
   const currentUser = users.u1; // Patient is logged in
 
   function sendMessage() {
+    currentChatId = 12;
     if (!input.trim()) return;
 
     const newMsg = {
