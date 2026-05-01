1d485ec92effe90a95c8affc4de5a5db31b3bd4c
fty <fruitfulnessomoragbon@gmail.com>
2026-05-01 21:34:31 +0100

    finishing loginchoice rerouting

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -58,7 +58,7 @@ export default function Chat() {
           <Link to="/reviews" style={{ textDecoration: "none" }}>
             <button style={{ backgroundColor: "#3b82f6", color: "white" }}>Reviews</button>
           </Link>
-          <Link to="/login" style={{ textDecoration: "none" }}>
+          <Link to="/login-choice" style={{ textDecoration: "none" }}>
             <button style={{ backgroundColor: "#3b82f6", color: "white" }}>Login</button>
           </Link>
           <Link to="/signup" style={{ textDecoration: "none" }}>
--- client/src/PostRequest.js
+++ client/src/PostRequest.js
@@ -38,7 +38,7 @@ function PostRequest() {
                 <Link to="/reviews" style={{ textDecoration: "none" }}>
                   <button style={{ backgroundColor: "#3b82f6", color: "white" }}>Reviews</button>
                 </Link>
-                <Link to="/login" style={{ textDecoration: "none" }}>
+                <Link to="/login-choice" style={{ textDecoration: "none" }}>
                   <button style={{ backgroundColor: "#3b82f6", color: "white" }}>Login</button>
                 </Link>
                 <Link to="/signup" style={{ textDecoration: "none" }}>
